136f8f05afdd05017e1069592fc599bd4047ee68
1Bernard <bernardansah5@gmail.com>
2025-11-25 10:21:17 +0000

    remove session store

--- config/initializers/session_store.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.configure do
-  config.session_store :cookie_store, key: "_todo_api_session"
-  config.middleware.use ActionDispatch::Cookies
-  config.middleware.use config.session_store, config.session_options
-end
